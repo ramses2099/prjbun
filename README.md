74fe4fe509d0089f0e7fa3cbbfdeb7fea528475e
ramses2099 <ramses2099@gmail.com>
2025-02-05 09:41:38 -0500

    first commit

--- README.md
+++ README.md
@@ -13,3 +13,28 @@ bun run index.ts
 ```
 
 This project was created using `bun init` in bun v1.2.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+# Init git
+
+```
+git init
+
+```
+
+# push to the local repository
+
+```
+git add .
+git commit -am "first commit"
+git push
+
+```
+
+# push to the server repository
+
+```
+git remote add origin https://github.com/ramses2099/prjbun.git
+git branch -M main
+git push -u origin main
+
+```
--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-console.log("Hello via Bun!");
+import figlet from "figlet";
+
+const server = Bun.serve({
+    port: 3000,
+    fetch: (req)=>{
+        const url = new URL(req.url);
+        if(url.pathname ==='/'){
+            const body = figlet.textSync("Hello world!");
+            return new Response(body);
+        }
+        //
+        if(url.pathname ==='/about'){
+            return new Response("About me!");
+        }
+        //
+        if(url.pathname ==='/feed'){
+            throw new Error('Could not fech feed');
+        }
+
+     
+
+        return new Response("404!");
+    },
+    error(error){
+        return new Response(`<pre>${error} \n ${error.stack} </pre>`,{
+            status: 500,
+            headers: {
+                'Content-Type': 'text/html'
+            }
+        });
+    }
+});
+
+console.log(`Listening on PORT http://localhost:${server.port}`);
